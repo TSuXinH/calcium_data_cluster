1e28dbae385557befc50f4d31754c09bb6edc03d
xxh_wit <861411454@qq.com>
2023-02-15 01:53:04 +0800

    test data1

--- test_for_data1.py
+++ test_for_data1.py
@@ -5,21 +5,81 @@ from utils import generate_firing_curve_config, visualize_firing_curves
 
 path = './data_alter/alter/bb001L-1.mat'
 file = h5py.File(path)
-Caa0 = file['CaA0']
+c0 = file['CaA0']
 
-cell_len = len(Caa0['celltype'][:].reshape(-1))
-f_df0 = Caa0[Caa0['F_dF'][:][0][0]][:].reshape(cell_len, -1)
 
-firing_curve_cluster_config = generate_firing_curve_config()
-firing_curve_cluster_config['mat'] = f_df0  # [rest_index]
-firing_curve_cluster_config['stim_kind'] = 'without'
-# firing_curve_cluster_config['multi_stim_index'] = trial1_stim_index
-firing_curve_cluster_config['show_part'] = 50
-firing_curve_cluster_config['axis'] = False
-firing_curve_cluster_config['raw_index'] = np.arange(cell_len)  # [rest_index]
-firing_curve_cluster_config['show_id'] = True
-# firing_curve_cluster_config['use_heatmap'] = True
-# firing_curve_cluster_config['h_clus'] = True
-# firing_curve_cluster_config['dist'] = 'euclidean'
-# firing_curve_cluster_config['method'] = 'ward'
-visualize_firing_curves(firing_curve_cluster_config)
+# firing_curve_cluster_config = generate_firing_curve_config()
+# firing_curve_cluster_config['mat'] = f_df0  # [rest_index]
+# firing_curve_cluster_config['stim_kind'] = 'without'
+# # firing_curve_cluster_config['multi_stim_index'] = trial1_stim_index
+# firing_curve_cluster_config['show_part'] = 50
+# firing_curve_cluster_config['axis'] = False
+# firing_curve_cluster_config['raw_index'] = np.arange(cell_len)  # [rest_index]
+# firing_curve_cluster_config['show_id'] = True
+# # firing_curve_cluster_config['use_heatmap'] = True
+# # firing_curve_cluster_config['h_clus'] = True
+# # firing_curve_cluster_config['dist'] = 'euclidean'
+# # firing_curve_cluster_config['method'] = 'ward'
+# visualize_firing_curves(firing_curve_cluster_config)
+
+
+idx_list = [1, 2, 3, 4, 5, 7, 8]
+for idx in idx_list:
+    print('current idx {}'.format(idx))
+    tmp_path = 'data_alter/alter/bb001L-{}.mat'.format(idx)
+    tmp_file = h5py.File(tmp_path)
+    c0 = tmp_file['CaA0']
+    c1 = tmp_file['CaA1']
+    cell_len = len(c0['celltype'][:].reshape(-1))
+    print(cell_len)
+    print(c0.keys())
+    print(c0['FOV'][:])
+    # f_df0 = c0[c0['F_dF'][:][0][0]][:].reshape(cell_len, -1)
+
+for item in c0['F_dF'][:][0]:
+    print(file[item][:].shape)
+
+for item in c0['trial_info'][:][0]:
+    print(file[item].keys())
+    break
+
+x = 1
+f_all = c0['F_dF'][:][0]  # shape: 454
+fdfx = c0[f_all[x]]
+ti0 = file[c0['trial_info'][:][0][0]]
+fl = ti0['fileloc'][:]
+mf = ti0['mat_file'][:]
+mm = ti0['motion_metric'][:]
+ts_ = ti0['time_stamp'][:]
+
+
+from util_for_data1 import fetch_trial_info
+res = fetch_trial_info(c0)
+for idx in range(len(res)):
+    print(res[idx][2].shape)
+
+licks = file['licks']
+lv = licks['lick_vector'][:][0]
+ts = licks['time_stamp'][:][0]
+t = licks['trial'][:][0]  # trivial
+
+
+for item in ts:
+    print(file[item][:])
+
+summary = file['summary']
+lo0 = summary['CaA0_leave_out'][:]
+lo1 = summary['CaA1_leave_out'][:]
+bd_ref = summary['bhv_dir'][:][0][0]
+bd = file[bd_ref][:]
+imgd = summary['img_dir'][:]
+table = summary['table'][:].transpose(1, 0)
+
+trials = file['trials']
+
+
+tt0 = table[0]
+for item in tt0:
+    print(file[item])
+
+print(licks[ts[0]], )
--- util_for_data1.py
+++ util_for_data1.py
@@ -1,8 +1,78 @@
 import h5py
 
+"""
+Raw file content:
+    CaA0, CaA1: shown below
+    
+    licks:
+    
+    summary:
+    
+    trials:
 
-def unzip_single_session(path):
-    file = h5py.File(path)
-    keys = list(file.keys())
-    if 'CaA0' in keys:
-        data = file['CaA0']
+CaA# content:
+    FOV: 200 488
+    
+    F_dF: the fluorescence traces, an N by 1 cell where N is the number of trials. 
+        Each cell contains an M x P double array where M is the number of ROIs and P is the number of timepoints in the n_th trial.
+        
+    ROIs: profile of the interested cell in the image.
+    
+    cellid: currently unknown
+    
+    celltype: 
+        the decoded celltypes of the recorded ROIs, an M x 1 double array where the number indicates the decoded cell type of the ROI.
+        
+    deconv: the deconvolved calcium events, an 1 by N cell where N is the number of trials.
+        Each cell contains an M x P double array where M is the number of ROIs and P is the number of timepoints in the n_th trial.
+        
+    Sampling rate: 32.5868
+    
+    trial_info: contains trial info necessary to link calcium recording with behavior data. 1 x N cell where N is the number of trials. 
+        CaA0.trial_info{n}.mat_file tells you the name of the nth trial which can be used to reference the correct trial in summary table (see summary table below)
+    
+
+    trial_info content:
+        fileloc: currently unknown
+        
+        mat_file: currently unknown
+        
+        motion_metric: currently unknown
+        
+        time_stamp: currently unknown
+
+
+licks: 
+    lick_vector: long vector
+    
+    time_stamp: different from the 'time_stamp' above
+    
+    trial: float number indicating n_th trial, like trial[0] -> 1.
+
+summary:
+    CaA0_leave_out:
+    
+    CaA1_leave_out:
+    
+    CaA2_leave_out:
+    
+    bhv_dir:
+    
+    img_dir:
+    
+    table:
+"""
+
+
+def fetch_trial_info(c):
+    ti_ref = c['trial_info'][:].reshape(-1)
+    result = []
+    for item in ti_ref:
+        tmp_ti = c[item]
+        fl = tmp_ti['fileloc'][:]
+        mf = tmp_ti['mat_file'][:]
+        mm = tmp_ti['motion_metric'][:]
+        ts = tmp_ti['time_stamp'][:]
+        ti_content = [fl, mf, mm, ts]
+        result.append(ti_content)
+    return result
